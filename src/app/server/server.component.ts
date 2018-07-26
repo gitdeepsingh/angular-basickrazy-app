2214ccc132821ee02dd6532ea454f710609e3a0b
Deep <deepsingh.cts@gmail.com>
2018-07-27 00:26:41 +0530

    string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-server',
-    templateUrl: './server.component.html'
+  selector: 'app-server',
+  templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'down';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
 }
