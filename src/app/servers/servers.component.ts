04fe396f117747e508b8b10ed463c799429cc273
Deep <deepsingh.cts@gmail.com>
2018-07-31 00:56:04 +0530

    the basic features added!

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,7 +15,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No server was created!";
-  serverName = '';
+  serverName = 'TestServer';
 
   constructor() {
     setTimeout(() => {
@@ -27,7 +27,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = "Server was created";
+    this.serverCreationStatus = "Server was created! Name is " + this.serverName;
   }
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
